948a143ca128a0fa2e26004fcef9ee0cda1bdcdc
Anuraj Nigade <96386849+AnurajNigade@users.noreply.github.com>
2023-12-14 18:43:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,115 @@
+import os
+
+import speech_recognition as sr
+import win32com.client
+import webbrowser
+import openai
+import datetime
+import random
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+chatStr = ""
+def chat(query):
+    global chatStr
+    # print(chatStr)  # Optional
+    openai.api_key = "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"
+    chatStr += f"Anuraj: {query}\n Jarvis: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    speaker.Speak(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
+
+
+def ai(prompt):
+    openai.api_key = "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"
+    text = f"OpenAI response for Prompt: {prompt} \n ********************\n\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # print(response["choices"][0]["text"])
+    text += response["choices"][0]["text"]
+    if not os.path.exists("Openai"):
+        os.mkdir("Openai")
+
+    # with open(f"Openai/prompt- {random.randint(1,2343434356)}.txt","w") as f:
+    with open(f"Openai/{''.join(prompt.split('ai')[1:]).strip() }.txt", "w") as f:
+        f.write(text)
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from Jarvis"
+
+
+
+if __name__ == '__main__':
+    speaker.Speak("Jarvis AI")
+    while 1:
+        print("Listening...")
+        query = takeCommand()
+        sites = [["youtube","https://www.youtube.com/"],
+               ["wikipedia","https://www.wikipedia.com"],
+               ["hackerrank","https://www.hackerrank.com/profile/anuraj_nigade"],
+               ["github","https://github.com/"],
+               ["leetcode","https://leetcode.com/Anuraj_Nigade/"],
+               ["chat gpt","https://chat.openai.com/"],
+                ["linkedin ", "https://www.linkedin.com/in/anurajnigade/"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                speaker.Speak(f"Opening {site[0]} Sir....")
+                webbrowser.open(site[1])
+
+        if "open music" in query:
+            music_path=r"D:\Song\LITE_BRITE.mp3"
+            os.startfile(music_path)
+
+        elif "the time" in query:
+            strfTimme = datetime.datetime.now().strftime("%H:%M:%S")
+            speaker.Speak(f"Sir the time is {strfTimme}")
+
+        elif "open vscode" in query:
+            os.startfile(r'C:\Users\user\AppData\Local\Programs\Microsoft VS Code\Code.exe')
+
+        elif "Using ai".lower() in query.lower():
+            ai(prompt=query)
+
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            chat(query)
+
+
+
+
+
+
+        # speaker.Speak(query)
---
+++ openaitest.py
@@ -0,0 +1,55 @@
+
+import os
+import openai
+
+
+openai.api_key = "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"
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Write an email to my boss for resignation?",
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+print(response)
+'''
+{
+  "warning": "This model version is deprecated. Migrate before January 4, 2024 to avoid disruption of service. Learn more https://platform.openai.com/docs/deprecations",
+  "id": "cmpl-8Ny9vwURq4i2UlXFJNqBanfZIwOyG",
+  "object": "text_completion",
+  "created": 1700723487,
+  "model": "text-davinci-003",
+  "choices": [
+    {
+      "text": "\n\nSubject: Resignation\n\nDear [Boss Name],\n\nI am writing to inform you of my intention to resign from my position at [Company Name]. My last day of work will be [date].\n\nI am grateful for the opportunity to work with the [Company Name] team and the valuable experience I have gained here. However, I have decided that the time is right for me to move on to the next stage of my career.\n\nI am committed to ensuring a smooth transition and am willing to stay on for the next [number] weeks to complete any outstanding projects or tasks.\n\nThank you again for the opportunity to work at [Company Name].\n\nSincerely,\n[Your Name]",
+      "index": 0,
+      "logprobs": null,
+      "finish_reason": "stop"
+    }
+  ],
+  "usage": {
+    "prompt_tokens": 9,
+    "completion_tokens": 150,
+    "total_tokens": 159
+  }
+}
+'''
+
+# import openai
+#
+# openai.api_key = "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"
+#
+# response = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "write email to ask for sick leave"}
+#     ]
+# )
+#
+# print(response['choices'][0]['message']['content'])
+
